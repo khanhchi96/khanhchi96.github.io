01cbfe8f0e61e6abfc8c6cc77f14c1fab928053a
Phung Khanh Chi <khanhchi2596@gmail.com>
2019-01-10 15:05:46 +0700

    local storage

---
+++ kanban board/script.js
@@ -0,0 +1,83 @@
+let COLUMN_TYPE = ['todo1', 'planning1', 'doing1', 'done1'];
+let DB = {
+    getData: function (){
+        if (typeof(Storage) !== "undefined") {
+    let data;
+    try {
+        data = JSON.parse(localStorage.getItem('list')) || {};
+    }catch(error){
+        data = {};
+    }
+
+    return data;
+    
+        } else {
+    alert('Sorry! No Web Storage support..');
+    return {};
+}
+    },
+    setData: function(data){
+        localStorage.setItem('list', JSON.stringify(data));
+
+    }
+}
+
+let list = DB.getData();
+
+
+        function deleteJob (span){
+           
+            let del = $(span).parent().parent();
+            $('#myModal').show();
+            $('#deleteconfirm').on('click', function(){
+                del.remove();
+                $('#myModal').hide();
+            });
+            $('#cancel, #close').on('click', function(){
+                $('#myModal').hide();
+            }); 
+        }
+
+
+        function newJob (obj){
+            let jobName = $('#' + `${obj.id}2`).val();
+            let item ='<div class="item"><div class="item-content"><span onclick="deleteJob(this)"><i class="fas fa-trash-alt"></i></span><p class="nd">' + jobName +'</p></div></div>';
+           if (!list[`${obj.id}1`]) list[`${obj.id}1`] = [];
+           list[`${obj.id}1`].push(item);
+           DB.setData(list);
+           
+            $('#' + `${obj.id}1`).append(item);
+            $('#' + `${obj.id}2`).val('')
+
+        }
+
+        function displayWordCounter(){
+                $('#done2').val($('#done2').val().substr(0, 62));
+                $('#doing2').val($('#doing2').val().substr(0, 62));
+                $('#planning2').val($('#planning2').val().substr(0, 62));
+                $('#todo2').val($('#todo2').val().substr(0, 62));
+                
+        }
+        
+        let app={newJob(obj){}, deleteJob(span){}}
+
+            $( function() {
+
+                COLUMN_TYPE.forEach(function(type){
+                    let columnType = list[type] || [];
+                    columnType.forEach(function(job){
+                        $('#' + type).append(job);
+                    })
+                });
+
+              $( ".sorted-list" ).sortable({
+                connectWith: ".sorted-list",
+                placeholder: "ui-state-highlight",
+                start: function(event, ui){
+                    $(ui.item[0]).addClass('dragging');
+                },
+                stop: function(event, ui){
+                    $(ui.item[0]).removeClass('dragging');
+                }
+              });
+            } );
